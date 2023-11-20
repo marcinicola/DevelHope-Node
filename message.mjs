83dbdec1d4b0a6c5ba45f2648255557bc8cdc3f9
Nicola Marci <nicola.marci1996@gmail.com>
2023-11-20 16:24:01 +0100

    Risoluzione Esercizio 8

--- message.mjs
+++ message.mjs
@@ -1,9 +1,6 @@
 import * as fs from "node:fs";
 
-const fileName = "example.txt";
-const fileContent = "Hello, Nico!";
-
-fs.writeFile(fileName, fileContent, (err) => {
+fs.writeFile("example.txt", "Hello,Nico!", (err) => {
   if (err) {
     console.error("Error file:", err);
   }
