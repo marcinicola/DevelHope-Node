bfc9c27a1a72e12acab63a6382f6b6e6118ab216
Nicola Marci <nicola.marci1996@gmail.com>
2023-11-22 18:16:06 +0100

    Risoluzione Esercizio 8

--- message.mjs
+++ message.mjs
@@ -4,4 +4,9 @@ fs.writeFile("example.txt", "Hello,Nico!", (err) => {
   if (err) {
     console.error("Error file:", err);
   }
+  fs.writeFile("example2.txt", "Hello,Nico Marci!", (err) => {
+    if (err) {
+      console.error("Error file:", err);
+    }
+  });
 });
